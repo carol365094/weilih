a84dd694db520f65c452a4bbaf33e8a090179d53
carol365094 <carol365094@gmail.com>
2020-06-11 02:21:29 +0800

    20061102

---
+++ layout/scripts/youyube.js
@@ -0,0 +1,15 @@
+/*
+Template Name: Wavefire
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Copyright: OS-Templates.com
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+File: Back to Top JS
+*/
+
+<script type="text/javascript">
+$(document).ready(function () {
+$('iframe[src*="youtube.com"]').wrap('<div class="youtubecontent" />');
+});
+</script>
