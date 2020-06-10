bc763ca6add3882e0fa969ef38bd173dd2391e3d
carol365094 <carol365094@gmail.com>
2020-06-11 00:24:13 +0800

    20061101

--- layout/scripts/jquery.mobilemenu.js
+++ layout/scripts/jquery.mobilemenu.js
@@ -13,3 +13,10 @@ Thanks to:
 */
 
 $('<form action="#"><select /></form>').appendTo("#mainav");$("<option />",{selected:"selected",value:"",text:"MENU"}).appendTo("#mainav select");$("#mainav a").each(function(){var e=$(this);if($(e).parents("ul ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul").length>=1){$("<option />",{value:e.attr("href"),text:""+e.text()}).appendTo("#mainav select")}else{$("<option />",{value:e.attr("href"),text:e.text()}).appendTo("#mainav select")}});$("#mainav select").change(function(){if($(this).find("option:selected").val()!=="#"){window.location=$(this).find("option:selected").val()}})
+$(function() {
+    $(".flexslider").flexslider({
+		slideshowSpeed: 5000, //展示时间间隔ms
+		animationSpeed: 500, //滚动时间ms
+		touch: true //是否支持触屏滑动
+	});
+});	
